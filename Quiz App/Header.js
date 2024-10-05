e5acda890a417ae4cca0648b17b5d15e12048fae
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-05 15:15:19 +0530

    Add files via upload

---
+++ Quiz App/Header.js
@@ -0,0 +1,11 @@
+import { GiBrain } from "react-icons/gi";
+function Header() {
+  return (
+    <header className="app-header">
+      <GiBrain color= "#f47344" size={80} />
+      <h1>Quiztion</h1>
+    </header>
+  );
+}
+
+export default Header;
